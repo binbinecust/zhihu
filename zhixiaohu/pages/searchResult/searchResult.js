a2807ac14e368b7838480d9cfe24788638a2b5ab
binbin.fang <binbin@yzyw-inc.com>
2018-11-14 00:34:07 +0800

    初始化

---
+++ zhixiaohu/pages/searchResult/searchResult.js
@@ -0,0 +1,26 @@
+Page({
+  data: {
+    search: ''
+  },
+  onLoad: function(options) {
+    //Do some initialize when page load.
+    this.setData({
+      search: options.search
+    });
+  },
+  onReady: function() {
+    //Do some when page ready.
+  },
+  onShow: function() {
+    //Do some when page show.
+  },
+  onHide: function() {
+    //Do some when page hide.
+  },
+  onUnload: function() {
+    //Do some when page unload.
+  },
+  onPullDownRefresh: function() {
+    //Do some when page pull down.
+  }
+});
